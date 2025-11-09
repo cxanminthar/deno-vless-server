667bf756387b72a5be56ee8fa140ce4d083da8bb
kaungsattkyaw <cxanminthar26@gmail.com>
2025-11-09 15:15:23 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const userID = Deno.env.get('UUID') || 'd342d11e-d424-4583-b36e-524ab1f0afa4'
+const userID = Deno.env.get('UUID') || '8e386203-d7cd-43fb-bfac-46acd5368ca7'
 const proxyIP = Deno.env.get('PROXYIP') || ''
 
 if (!isValidUUID(userID)) {
